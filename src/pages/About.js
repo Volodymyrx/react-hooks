7f99de28c2290f6ffa0a761af99f9f679bc29fcd
Volodymyrx <volodiax@mail.ru>
2020-05-06 17:47:04 +0300

    finish some clean

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,23 +7,22 @@ export const About = () => {
         <div>
             <h1>About project</h1>
             <p>Next lesson project by course "React Hooks" </p>
-            <p>Teacher
-                <NavLink to='/profile/vladilenm'><strong>Vladilen Minin</strong></NavLink>
+            <p>Teacher:
+                <NavLink to='/profile/vladilenm'><strong>&nbsp; Vladilen Minin</strong></NavLink>
             </p>
-            <p>My profile
-                <NavLink to='/profile/volodymyrx'><strong>Volodymyr Martynenko</strong></NavLink>
+            <p>My profile:
+                <NavLink to='/profile/volodymyrx'><strong> &nbsp; Volodymyr Martynenko</strong></NavLink>
             </p>
             <p>volodiax@ukr.net </p>
-            <p> This project on GitHub:
-                <h5>
-                    <a
-                        href='https://github.com/Volodymyrx/react-hooks'
-                        target='_blank'
-                        rel="noopener noreferrer"
-                    >react-hooks
-                    </a>
-                </h5>
-            </p>
+            <p> This project on GitHub:</p>
+            <h5>
+                <a
+                    href='https://github.com/Volodymyrx/react-hooks'
+                    target='_blank'
+                    rel="noopener noreferrer"
+                >react-hooks
+                </a>
+            </h5>
         </div>
     )
 };
